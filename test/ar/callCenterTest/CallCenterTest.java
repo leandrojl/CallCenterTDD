4d9acc7dbe6e90b15b85ce30d4634f09ae98cc88
leandrojl <leandroracing96@hotmail.com>
2022-09-21 21:02:36 -0300

    Testing first commit with SSH and using linux terminal to upload file in GIthub

---
+++ test/ar/callCenterTest/CallCenterTest.java
@@ -0,0 +1,14 @@
+package ar.callCenterTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CallCenterTest {
+//GIVEN - WHEN - THEN for unit testing
+	@Test
+	public void test() {
+		
+	}
+
+}
